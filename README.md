865231a59ae64c7c5e75ee0a2c2b492b7486f1b3
Ben Barton <50291343+benrussellbarton@users.noreply.github.com>
2020-03-08 15:48:33 +0000

    Add files via upload
    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio
+Ben Barton Portfolio
---
+++ js/typed.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+  var typed = new Typed('#typed',{
+    stringsElement: '#typed-strings',
+    typeSpeed: 70,
+    backSpeed: 30,
+    backDelay: 2000,
+    startDelay: 100,
+    loop: true,
+  });
+});
